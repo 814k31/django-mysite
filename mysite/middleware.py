e3f1565dc24c98e816fc31701d4d9e422e38a649
Blake Lovelace <lovelace.b@hotmail.com>
2024-03-24 17:12:24 +0800

    Clean up

--- mysite/middleware.py
+++ mysite/middleware.py
@@ -5,11 +5,9 @@ from cryptography.x509 import load_pem_x509_certificate
 from django.http import JsonResponse
 
 def validate_jwt(token, audience, issuer):
-    # Fetch the JWKS from the provided URL
     response = requests.get(issuer + '.well-known/jwks.json')
     jwks = response.json()
 
-    # Find the correct RSA key in the JWKS
     rsa_key = None
     for key in jwks['keys']:
         if key['kty'] == 'RSA' and key['alg'] == 'RS256':
@@ -18,7 +16,6 @@ def validate_jwt(token, audience, issuer):
     else:
         raise ValueError("No RSA public key found in JWKS")
 
-    # Parse the RSA public key from JWKS
     cert_str = "-----BEGIN CERTIFICATE-----\n" + rsa_key['x5c'][0] + "\n-----END CERTIFICATE-----"
     cert_obj = load_pem_x509_certificate(cert_str.encode(), default_backend())
     public_key = cert_obj.public_key()
@@ -30,17 +27,13 @@ def validate_jwt(token, audience, issuer):
 
 def token_validation_middleware(get_response):
     def middleware(request):
-        # Retrieve the access token from the request headers
         authorisation_header = request.headers.get('Authorization', '')
 
         if (authorisation_header == ""):
-            # response = get_response(request)
-            # return response
             return JsonResponse({'error': 'No Authorization Header'}, status=401)
 
         access_token = authorisation_header.split(' ')[1]
         try:
-            # Validate the access token
             validate_jwt(
                 access_token,
                 issuer='https://dev-yxuqagk74bpowmp5.us.auth0.com/',
